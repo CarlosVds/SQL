e85d824d0af7e5a77e7895af09ed1c34c5ca77b9
CarlosVds <carloseduardovds@gmail.com>
2026-07-08 20:46:43 -0300

    Acrescentando mais filtros com case when na query

--- 13-Case when/exercicioCaseWhen.sql
+++ 13-Case when/exercicioCaseWhen.sql
@@ -1,8 +1,14 @@
 select 
-seller_city,
-seller_state,
-    case 
-        when seller_state = 'SP' then 'Sao Paulo'
-    else 'Sem identificação'
-    end as 'Estado'   
+distinct
+    seller_city,
+    count(seller_city),
+    seller_state,
+        case 
+            when seller_state = 'SP' then 'Sao Paulo'
+            when seller_state = 'RJ' then 'Rio de Janeiro'
+            when seller_state = 'SP' then 'Sao Paulo'
+        else 'Sem identificação'
+        end as 'Estado'   
 from tb_sellers
+group by 1
+order by 2 desc 
